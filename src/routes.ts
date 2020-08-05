eb85d3fbc34199a49b7c83b667f2b404739be96f
Bruna de P. M <brunamonarin@gmail.com>
2020-08-05 01:26:05 -0300

    Create route to list classes

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import ClassesController from './controllers/ClassesControllers';
 const routes = express.Router();
 const classesControllers = new ClassesController();
 
+routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
 
 export default routes;
