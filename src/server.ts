e6195c0954d4299aecd807c2ef2f973151380ab6
Bruna de P. M <brunamonarin@gmail.com>
2020-08-05 00:37:17 -0300

    Create database

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello NLW2' })
-});
+app.use(routes);
 
 app.listen(3333);
 
